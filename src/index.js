132e6a62256c0198439249b5566abcdee40ef3b0
teke <mutau85@gmail.com>
2022-09-30 21:18:32 +0200

    run linter check

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import './style.css';
-import titleIcon from './icons/refresh.png';
-import addIcon from './icons/add.png';
-import showList from './modules/showListItems.js';
+/* eslint-disable import/no-unresolved */
+import './index.css'
+import titleIcon from './icons/refresh.png'
+import addIcon from './icons/add.png'
+import ToDo from './modules/manageListItems.js'
+import check from './modules/checkBox.js'
 
-showList();
+const titleSpan = document.getElementById('title-icon')
+const addSpan = document.getElementById('add-icon')
+const icon1 = new Image()
+icon1.src = titleIcon
+icon1.classList.add('reset')
+const icon2 = new Image()
+icon2.src = addIcon
+titleSpan.appendChild(icon1)
+addSpan.appendChild(icon2)
 
-const titleSpan = document.getElementById('t-icon');
-const addSpan = document.getElementById('add-icon');
-const icon1 = new Image();
-icon1.src = titleIcon;
-const icon2 = new Image();
-icon2.src = addIcon;
-titleSpan.appendChild(icon1);
-addSpan.appendChild(icon2);
+ToDo.add()
+ToDo.show()
+ToDo.remove()
+ToDo.edit()
+ToDo.clear()
+check()
+ToDo.reset()
